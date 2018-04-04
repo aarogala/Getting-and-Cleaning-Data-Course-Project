4ca14bcb462bb5873f3a1e72e93c0e0146a2dc9d
aarogala <32551059+aarogala@users.noreply.github.com>
2018-04-04 19:22:19 +0200

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+setwd('C:/Users/Ola/Dysk Google/Coursera/Getting and cleaning data/projekt')
+getwd()
+
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip',
+              destfile = 'C:/Users/Ola/Desktop/temp/Samsung.zip')
+
+labels_test<-read.table('UCI HAR Dataset/test/y_test.txt')
+labels_train<-read.table('UCI HAR Dataset/train/y_train.txt')
+test<-read.table('UCI HAR Dataset/test/x_test.txt')
+train<-read.table('UCI HAR Dataset/train/x_train.txt')
+features<-read.table('UCI HAR Dataset/features.txt')
+activity_labels<-read.table('UCI HAR Dataset/activity_labels.txt')
+subject_test<-read.table('UCI HAR Dataset/test/subject_test.txt')
+subject_train<-read.table('UCI HAR Dataset/train/subject_train.txt')
+
+# dim(subject_test)
+# dim(subject_train)
+# 
+# head(subject_test)
+# head(subject_train)
+# 
+# table(subject_test[,1])
+# table(subject_train[,1])
+# 
+# dim(labels_test)
+# dim(test)
+# 
+# dim(labels_train)
+# dim(train)
+
+test_labeled<-cbind(test,labels_test,subject_test)
+train_labeled<-cbind(train,labels_train,subject_train)
+
+# dim(test_labeled)
+# dim(train_labeled)
+
+data<-rbind(test_labeled,train_labeled)
+
+# dim(data)
+
+features[,2]<-gsub('\\(\\)','',features[,2])
+features[,2]<-gsub('-','_',features[,2])
+
+colnames(data)<-c(as.character(features[,2]),'label','subject')
+
+library(plyr)
+data$activity_labels<-mapvalues(data$label,from=activity_labels[,1],to=as.character(activity_labels[,2]))
+
+# dim(data)
+data_reduced<-data[,c(grep('mean',colnames(data)), grep('std',colnames(data)),562:564)]
+
+library(dplyr)
+analysis<-ddply(data_reduced,.(activity_labels,subject),numcolwise(mean))
+
+write.table(analysis,'analysis.txt',row.name=FALSE)
+
